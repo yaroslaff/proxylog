c89afe536da4460d026e53cf58c218e2b41da340
Yaroslav <yaroslaff@gmail.com>
2021-06-01 05:04:28 +0700

    initial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# yxorp
+# Proxyca
+Proxy, Check and Alert
---
+++ proxyca.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+import argparse
+import dotenv
+import os
+import urllib.parse
+
+import aiohttp
+import aiohttp.web
+import requests
+
+target = None
+target_host = None
+
+def get_args():
+
+    def_target = os.getenv('TARGET')
+
+    parser = argparse.ArgumentParser(description='reverse proxy')
+    parser.add_argument('-t', '--target', default=def_target,
+        help=f'Target website, e.g. http://google.com  ({def_target})')
+
+    return parser.parse_args()
+
+async def handle(request):
+    url = target + request.path_qs
+    print(f"<{os.getpid()}> forward to {url}")
+
+    in_headers = dict(request.headers)
+    in_headers['Host'] = target_host
+
+    if request.method == 'GET':
+        async with aiohttp.request('GET', url) as response:
+            payload = await response.read()
+            out_headers = dict(response.headers)
+            
+            for k in ['Content-Encoding', 'Content-Length']:
+                if k in out_headers:
+                    del(out_headers[k])
+
+            print(f"<{os.getpid()}> return {url}")
+
+            return aiohttp.web.Response(body = payload,
+                headers = out_headers, 
+                status=response.status)
+            #return response
+
+        print("ZZZZZZZZZZZz")
+    elif request.method == 'HEAD':
+        r = requests.head(url)
+    elif request.method == 'POST':
+        r = requests.post(url)
+    else:
+        print(f"!!! ZZZZZ unsupported method {request.method}")
+
+
+    payload = ('x'*50 + '\n')*10
+    print("QQQQQQQQQQQQQQQQQQq")
+    return aiohttp.web.Response(body = payload, 
+        status=r.status_code, 
+        headers=r.headers)
+
+def main():
+    global target, target_host
+    args = get_args()
+    target = args.target
+    target_host = urllib.parse.urlparse(target).netloc
+
+    app = aiohttp.web.Application()
+    app.add_routes([ aiohttp.web.get('/{tail:.*}', handle) ])
+    aiohttp.web.run_app(app)
+
+main()
